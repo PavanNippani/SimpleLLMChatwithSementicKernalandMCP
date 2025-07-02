e30031baa9fa1caee4c1ada8dc2ccfd1de2fe62b
Pavan  Nippani <pavann@microsoft.com>
2025-07-02 16:06:03 -0400

    Add Program.cs for session and Razor Pages setup

---
+++ Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddSession();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseSession();
+
+app.MapRazorPages();
+
+app.Run();
